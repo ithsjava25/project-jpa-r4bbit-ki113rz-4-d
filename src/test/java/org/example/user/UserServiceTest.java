c369ad0d252b120dd41cc2bfa5566ea1828af4c3
Fiona Friberg <fionafriberg@outlook.com>
2025-12-16 13:51:27 +0100

    Add test shouldCreateUser

--- src/test/java/org/example/user/UserServiceTest.java
+++ src/test/java/org/example/user/UserServiceTest.java
@@ -6,12 +6,25 @@ import org.example.*;
 import org.hibernate.jpa.HibernatePersistenceConfiguration;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Test;
+import static org.assertj.core.api.Assertions.assertThat;
 
 public class UserServiceTest {
 
     private EntityManagerFactory emf;
     private UserService userService;
 
+
+    /**
+     * Körs före varje test.
+     *
+     * Metoden:
+     * - Konfigurerar Hibernate med MySQL
+     * - Skapar EntityManagerFactory
+     * - Initierar UserRepository och UserService
+     *
+     * Syftet är att varje test ska starta med en korrekt konfigurerad testmiljö.
+     */
     @BeforeEach
     void setUp() {
 
@@ -29,11 +42,34 @@ public class UserServiceTest {
 
     }
 
+    /**
+     * Körs efter varje test.
+     *
+     * Stänger EntityManagerFactory och frigör databaskopplingar
+     * för att undvika minnesläckor och låsta resurser.
+     */
     @AfterEach
     void tearDown() {
         if (emf != null) {
             emf.close();
         }
+
+    }
+    @Test
+    void shouldCreateUser() {
+        //given
+        String firstName = "John";
+        String lastName = "Doe";
+        String password = "1234";
+
+        //when
+        User user = userService.createUser(firstName, lastName, password);
+
+        //then
+        assertThat(user).isNotNull();
+        assertThat(user.getUserId()).isNotNull();
+        assertThat(user.getUsername()).isEqualTo("JohDoe");
+        assertThat(user.getPassword()).isEqualTo("1234");
 
     }
 }
