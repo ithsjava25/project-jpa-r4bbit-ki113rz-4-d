c705e5556a22b1e5ecdbf4b2faf9c903a47b6aa2
Fiona Friberg <fionafriberg@outlook.com>
2025-12-16 13:27:44 +0100

    Add initial UserService test setup

--- src/test/java/org/example/user/UserServiceTest.java
+++ src/test/java/org/example/user/UserServiceTest.java
@@ -1,6 +1,39 @@
 package org.example.user;
 
-import org.junit.jupiter.api.Test;
+import jakarta.persistence.EntityManagerFactory;
+
+import org.example.*;
+import org.hibernate.jpa.HibernatePersistenceConfiguration;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.AfterEach;
 
 public class UserServiceTest {
+
+    private EntityManagerFactory emf;
+    private UserService userService;
+
+    @BeforeEach
+    void setUp() {
+
+        var cfg =
+            new HibernatePersistenceConfiguration("emf")
+                .jdbcUrl("jdbc:mysql://localhost:3306/bulletin")
+                .jdbcUsername("root")
+                .jdbcPassword("root")
+                .property("hibernate.hbm2ddl.auto", "update")
+                .managedClasses(User.class);
+
+        emf = cfg.createEntityManagerFactory();
+        var userRepo = new UserRepositoryImpl(emf);
+        userService = new UserServiceImpl(userRepo);
+
+    }
+
+    @AfterEach
+    void tearDown() {
+        if (emf != null) {
+            emf.close();
+        }
+
+    }
 }
