d2f4f4f1f959b7fd86fdee2f9484b4b45a1ecb5d
Fiona Friberg <fionafriberg@outlook.com>
2025-12-16 15:22:57 +0100

    Add test validateUser

--- src/test/java/org/example/user/UserServiceTest.java
+++ src/test/java/org/example/user/UserServiceTest.java
@@ -58,9 +58,9 @@ public class UserServiceTest {
     @Test
     void shouldCreateUser() {
         //given
-        String firstName = "John";
-        String lastName = "Doe";
-        String password = "1234";
+        String firstName = "Fiffen";
+        String lastName = "Friberg";
+        String password = "ImAlwaysOneWeekBehind";
 
         //when
         User user = userService.createUser(firstName, lastName, password);
@@ -68,8 +68,26 @@ public class UserServiceTest {
         //then
         assertThat(user).isNotNull();
         assertThat(user.getUserId()).isNotNull();
-        assertThat(user.getUsername()).isEqualTo("JohDoe");
-        assertThat(user.getPassword()).isEqualTo("1234");
+        assertThat(user.getUsername()).isEqualTo("FifFri");
+        assertThat(user.getPassword()).isEqualTo("ImAlwaysOneWeekBehind");
 
+    }
+
+    @Test
+    void validateUser() {
+        //given
+        String firstName = "Sandra";
+        String lastName = "Neljestam";
+        String password = "ILoveHouseFlipper";
+
+        User created = userService.createUser(firstName, lastName, password);
+        String username = created.getUsername();
+
+        //when
+        User validated = userService.validateUser(username, password);
+
+        //then
+        assertThat(validated).isNotNull();
+        assertThat(validated.getUsername()).isEqualTo(username);
     }
 }
