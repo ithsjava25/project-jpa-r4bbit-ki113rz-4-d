fba510137e39f92248dc65b4dec12a3b79617658
Daniel Marton <daniel.marton91@gmail.com>
2025-12-18 10:36:39 +0100

    Moved LoginController to Controllers again

--- src/main/java/org/example/Controllers/LoginController.java
+++ src/main/java/org/example/Controllers/LoginController.java
@@ -1,14 +1,11 @@
-package org.example.Controllers;
+package org.example;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
-import org.example.App;
-import org.example.Services.PostService;
 import org.example.Services.UserService;
-import org.example.UserSession;
 
 /**
  * Class to link LoginView to Controller
