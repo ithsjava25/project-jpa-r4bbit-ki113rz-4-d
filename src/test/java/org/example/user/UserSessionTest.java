27bc6f2f9a22ffea7e655fa4a8e0600c0ab31328
Fiona Friberg <fionafriberg@outlook.com>
2025-12-22 15:00:13 +0100

    Add tests to UserSessionTest

--- src/test/java/org/example/user/UserSessionTest.java
+++ src/test/java/org/example/user/UserSessionTest.java
@@ -1,7 +1,62 @@
 package org.example.user;
 
-public class UserSessionTest {
+import org.example.Entities.User;
+import org.example.UserSession;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Test;
+import static org.assertj.core.api.Assertions.assertThat;
 
 
 
-}
+class UserSessionTest {
+
+    @AfterEach
+    void clearSession() {
+        UserSession.logout();
+    }
+
+
+
+    /**
+     * Verifies that
+     *      - session is empty at start
+     */
+    @Test
+    void shouldNotHaveUserLoggedInInitially() {
+        assertThat(UserSession.isLoggedIn()).isFalse();
+        assertThat(UserSession.getCurrentUser()).isEmpty();
+    }
+
+    /**
+     * Verifies that
+     *      - session gets current user when logged in
+     */
+    @Test
+    void shouldLoginUser() {
+        User user = new User("Scooby", "Doo", "ScoobySnaxxx", "scoobydoobydoo");
+
+        UserSession.login(user);
+
+        assertThat(UserSession.isLoggedIn()).isTrue();
+        assertThat(UserSession.getCurrentUser()).contains(user);
+    }
+
+    /**
+     * Verifies that
+     *      - currentUser is set to null when logged out
+     */
+        @Test
+        void shouldLogoutUserFromSession() {
+            User user = new User("Norville", "Rogers", "Shaggy", "password123");
+
+            UserSession.login(user);
+            assertThat(UserSession.isLoggedIn()).isTrue();
+
+            UserSession.logout();
+
+            assertThat(UserSession.isLoggedIn()).isFalse();
+            assertThat(UserSession.getCurrentUser()).isEmpty();
+        }
+
+    }
+
