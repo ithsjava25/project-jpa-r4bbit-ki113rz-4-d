34038baa9f31de8204e329a080c0758c4249b6c6
Fiona Friberg <fionafriberg@outlook.com>
2026-01-07 17:38:03 +0100

    add posts table

--- src/main/resources/db/BulletinDatabase.sql
+++ src/main/resources/db/BulletinDatabase.sql
@@ -13,3 +13,13 @@ create table if not exists categories (
     categoryId bigint auto_increment primary key ,
     name varchar(255) not null unique
 );
+
+create table if not exists posts (
+    postId bigint auto_increment primary key ,
+    postit_color varchar(255) not null ,
+    subject varchar(255) not null ,
+    message varchar(255) not null ,
+    created_at datetime not null ,
+    user_id bigint not null ,
+    foreign key (user_id) references user_account(userId) on delete cascade
+);
