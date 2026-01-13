c330c53e4caf7c4aa62bd02ab8437c9696c3cff0
Fiona Friberg <fionafriberg@outlook.com>
2026-01-13 18:53:05 +0100

    Add test createCategory

--- src/test/java/org/example/category/CategoryServiceTest.java
+++ src/test/java/org/example/category/CategoryServiceTest.java
@@ -8,10 +8,13 @@ import org.example.Repositories.CategoryRepositoryImpl;
 import org.example.Services.CategoryService;
 import org.example.Services.CategoryServiceImpl;
 import org.hibernate.jpa.HibernatePersistenceConfiguration;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.condition.DisabledOnOs;
 import org.junit.jupiter.api.condition.OS;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.example.JPAUtil.emf;
 
 @DisabledOnOs(OS.LINUX)
@@ -34,4 +37,30 @@ public class CategoryServiceTest {
         categoryService = new CategoryServiceImpl(new CategoryRepositoryImpl(emf));
 
     }
+
+    @AfterEach
+    public void tearDown()
+    {
+        emf.close();
+    }
+
+    @Test
+    public void createCategory() {
+        Category category = categoryService.createCategory("General");
+
+        assertThat(category).isNotNull();
+        assertThat(category.getCategoryId()).isNotNull();
+
+    }
+
+    @Test
+    public void findCategoryById() {
+
+    }
+
+    @Test
+    public void findAllCategories() {
+
+    }
+
 }
